27f2529c500c7a9fba737d36ae4b26ac2751f056
remyduijn <remy.duijn@gmail.com>
2022-03-31 14:20:52 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome, {props.name}
         </p>
         <a
           className="App-link"
